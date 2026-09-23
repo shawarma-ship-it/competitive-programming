ee8000259a84d575e291bbe3f218089ca7519dea
shawarma-ship-it <root@LAPTOP-OJV9OFOE.localdomain>
2026-09-23 17:03:44 +0000

    template and mistakes log

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    // per-test-case logic
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;              // delete this line for single-test problems
+    while (t--) solve();
+    return 0;
+}
